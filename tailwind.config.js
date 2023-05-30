fe3e0d8e97070b62e57214aef49309470f6e0e61
sunny! <66428984+sunnymakespromises@users.noreply.github.com>
2023-05-30 16:02:53 -0400

    housekeeping + README

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,14 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
     content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
+        "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
-      extend: {
-        fontFamily: {
-            main: ['FOT-UDMarugo_Large Pr6N', ...defaultTheme.fontFamily.sans],
+        extend: {
+            fontFamily: {
+                main: ['FOT-UDMarugo_Large Pr6N', ...defaultTheme.fontFamily.sans],
+            },
         },
-      },
     },
     plugins: [],
 }
