eea1e395b7630354a700a162ed76702a16994496
sunny! <66428984+sunnymakespromises@users.noreply.github.com>
2023-06-03 09:13:55 -0400

    graphical changes

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,10 +1,14 @@
 import { useRootContext } from '../contexts/root'
+import { Helmet } from 'react-helmet'
 
 export default function Home() {
     const { user } = useRootContext()
 
     return (
         <div>
+            <Helmet>
+                <title>dashboard | betsy</title>
+            </Helmet>
 
         </div>
     )
--- src/routes/settings.jsx
+++ src/routes/settings.jsx
@@ -21,16 +21,16 @@ export default function Settings() {
             <Helmet>
                 <title>settings | betsy</title>
             </Helmet>
-            <Text classes = 'w-full flex flex-row justify-end !text-5xl !font-extrabold whitespace-nowrap'>
+            <Text classes = 'w-full flex flex-row italic !text-5xl !font-extrabold whitespace-nowrap'>
                 settings
             </Text>
             <div id = 'settings-container' className = 'h-full w-full rounded-main dark:backdrop-brightness-light backdrop-brightness-dark flex flex-col gap-4 p-4'>
                 <div id = 'settings' className = 'w-full h-full flex flex-col items-center overflow-scroll scroll-smooth gap-2'>
                     <Image path = {user ? user.picture : ''} classes = 'h-48 aspect-square rounded-full'/>
-                    <div id = 'username-container' className = ''>
-
-                    </div>
-                    <div id = 'favorites' className = ''>
+                    <Text classes = 'font-bold'>
+                        {user ? user.username : ''}
+                    </Text>
+                    <div id = 'favorites' className = 'h-min w-full'>
 
                     </div>
                     <Button classes = 'min-w-[20%]' onClick = {signout}>
