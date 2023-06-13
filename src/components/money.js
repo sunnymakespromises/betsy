06dc55ca5ba9d2a4f1fa46a582f249b91bf63c64
sunny! <66428984+sunnymakespromises@users.noreply.github.com>
2023-06-13 04:50:06 -0400

    take away coin pic from money.js

--- src/components/money.js
+++ src/components/money.js
@@ -4,30 +4,30 @@ import Text from './text'
 
 export default function Money({amount, classes, textClasses, ...extras}) {
     const textRef = useRef()
-    const [coinHeight, setCoinHeight] = useState(0)
+    // const [coinHeight, setCoinHeight] = useState(0)
 
-    useEffect(() => {
-        getHeight()
-    }, [textRef?.current?.clientHeight, amount])
+    // useEffect(() => {
+    //     getHeight()
+    // }, [textRef?.current?.clientHeight, amount])
 
-    useEffect(() => {
-        if (typeof window === 'undefined') {
-            return
-        }
-        window.addEventListener('resize', getHeight)
-        return () => window.removeEventListener('resize', getHeight)
-    }, [])
+    // useEffect(() => {
+    //     if (typeof window === 'undefined') {
+    //         return
+    //     }
+    //     window.addEventListener('resize', getHeight)
+    //     return () => window.removeEventListener('resize', getHeight)
+    // }, [])
 
-    function getHeight() {
-        setCoinHeight((getComputedStyle(textRef?.current).height.replace('px', '') * 0.7) + 'px')
-    }
+    // function getHeight() {
+    //     setCoinHeight((getComputedStyle(textRef?.current).height.replace('px', '') * 0.7) + 'px')
+    // }
     
     return (
         <div ref = {textRef} className = {'money-container flex flex-row items-center gap-tiny md:gap-small' + (classes ? ' ' + classes : '')} {...extras}>
             <Text classes = {'money-value' + (textClasses ? ' ' + textClasses : '')}>
-                {amount ? amount : '0.00'}
+                {'$' + (amount ? amount : '0.00')}
             </Text>
-            <Image path = {'images/coin.svg'} classes = 'money-coin aspect-square' styles = {{height: coinHeight}}/>
+            {/* <Image path = {'images/coin.svg'} classes = 'money-coin aspect-square' styles = {{height: coinHeight}}/> */}
         </div>
     )
 }
