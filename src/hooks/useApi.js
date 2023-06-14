7af4df504e86f7ba543962a8357adc4e1d1e15ff
sunny! <66428984+sunnymakespromises@users.noreply.github.com>
2023-06-14 05:08:50 -0400

    dont need to pass odds format to initializeApiData

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,11 +1,9 @@
-import { useWindowContext } from '../contexts/window'
 import { initializeApiData as _initializeApiData } from '../lib/api/initializeApiData'
 
 
 function useApi() {
-    const { cookies } = useWindowContext()
     async function initializeApiData() {
-        const response = await _initializeApiData(cookies['odds-format'])
+        const response = await _initializeApiData()
     }
 
     return { initializeApiData }
