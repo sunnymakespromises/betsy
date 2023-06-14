ba7f0345a1616ec5b1ad9eba2a59a32086c26d1e
sunny! <66428984+sunnymakespromises@users.noreply.github.com>
2023-06-14 00:31:13 -0400

    added more complex getItem support (non-pk)

--- src/lib/aws/db/getItem.js
+++ src/lib/aws/db/getItem.js
@@ -1,17 +1,21 @@
 import { ddbDocClient } from './ddbDocClient'
 import { GetCommand } from '@aws-sdk/lib-dynamodb'
+import queryTable from './queryTable'
 
 export default async function getItem(table, key) {
-    try {
-        const params = {
-            TableName: 'Betsy_' + table,
-            Key: {
-                id: key
+    if (key.constructor.name === 'Object') {
+        return await queryTable(table, key, true)
+    }
+    else {
+        try {
+            const params = {
+                TableName: 'Betsy_' + table,
+                Key: key.constructor.name === 'Object' ? key : { id: key }
             }
+            const { Item } = await ddbDocClient.send(new GetCommand(params))
+            return Item
+        } catch (err) {
+            console.log('Error', err)
         }
-        const { Item } = await ddbDocClient.send(new GetCommand(params))
-        return Item
-    } catch (err) {
-        console.log('Error', err)
     }
 }
