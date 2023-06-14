8f2d40db74095e9b2f1791c553f095e554bfafb6
sunny! <66428984+sunnymakespromises@users.noreply.github.com>
2023-06-14 00:37:07 -0400

    added more complex deleteItem support

--- src/lib/aws/db/deleteItem.js
+++ src/lib/aws/db/deleteItem.js
@@ -1,16 +1,29 @@
 import { ddbDocClient } from './ddbDocClient'
 import { DeleteCommand } from '@aws-sdk/lib-dynamodb'
+import queryTable from './queryTable'
 
 export default async function deleteItem(table, key) {
-    const params = {
-        TableName: 'Betsy_' + table,
-        Key: {
-            id: key
+    if (key.constructor.name === 'Object') {
+        try {
+            const item = await queryTable(table, key, true)
+            const params = {
+                TableName: 'Betsy_' + table,
+                Key: { id: item.id }
+            }
+            await ddbDocClient.send(new DeleteCommand(params))
+        } catch (err) {
+            console.log('Error', err.stack)
         }
     }
-    try {
-        await ddbDocClient.send(new DeleteCommand(params))
-    } catch (err) {
-        console.log('Error', err.stack)
+    else {
+        try {
+            const params = {
+                TableName: 'Betsy_' + table,
+                Key: { id: key }
+            }
+            await ddbDocClient.send(new DeleteCommand(params))
+        } catch (err) {
+            console.log('Error', err.stack)
+        }
     }
 }
