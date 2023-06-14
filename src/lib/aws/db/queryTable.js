a157a522be0b6016b2e6f6b885b2a8b2cd2e3c0c
sunny! <66428984+sunnymakespromises@users.noreply.github.com>
2023-06-14 00:10:17 -0400

    added reserved keyword support

--- src/lib/aws/db/queryTable.js
+++ src/lib/aws/db/queryTable.js
@@ -1,16 +1,23 @@
 import { ddbDocClient } from './ddbDocClient'
 import { ScanCommand } from '@aws-sdk/lib-dynamodb'
+import reserved_keywords from './aws_reserved_keywords'
 
 export default async function queryTable(table, query) {
     try {
         let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
         let letterIndex = 0
         let filterExpression = ''
+        let expressionAttributeNames = {}
         let expressionAttributeValues = {}
         for (let updateKey of Object.keys(query)) {
+            const keyIsReserved = reserved_keywords.includes(updateKey)
             let comma = Object.keys(query).indexOf(updateKey) !== Object.keys(query).length - 1 ? ' and ' : ''
-            filterExpression += updateKey + ' = :' + letters[letterIndex] + comma
+            let pound = keyIsReserved ? '#' : ''
+            filterExpression += pound + updateKey + ' = :' + letters[letterIndex] + comma
             expressionAttributeValues[':' + letters[letterIndex]] = query[updateKey]
+            if (keyIsReserved) {
+                expressionAttributeNames['#' + updateKey] = updateKey
+            }
             letterIndex++
         }
         const params = {
@@ -19,7 +26,9 @@ export default async function queryTable(table, query) {
             ExpressionAttributeValues: expressionAttributeValues,
             ConsistentRead: true
         }
-
+        if (Object.keys(expressionAttributeNames).length !== 0) {
+            params['ExpressionAttributeNames'] = expressionAttributeNames
+        }
         const { Items } = await ddbDocClient.send(new ScanCommand(params))
         return Items
     } catch (err) {
--- src/lib/aws/db/updateItem.js
+++ src/lib/aws/db/updateItem.js
@@ -1,24 +1,32 @@
 import { ddbDocClient } from './ddbDocClient'
 import { UpdateCommand } from '@aws-sdk/lib-dynamodb'
+import reserved_keywords from './aws_reserved_keywords'
 
 export default async function updateItem(table, key, object) {
     let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     let letterIndex = 0
     let updateExpression = 'set '
+    let expressionAttributeNames = {}
     let expressionAttributeValues = {}
     for (let updateKey of Object.keys(object)) {
+        const keyIsReserved = reserved_keywords.includes(updateKey)
         let comma = Object.keys(object).indexOf(updateKey) !== Object.keys(object).length - 1 ? ', ' : ''
-        updateExpression += updateKey + ' = :' + letters[letterIndex] + comma
+        let pound = keyIsReserved ? '#' : ''
+        updateExpression += pound + updateKey + ' = :' + letters[letterIndex] + comma
         expressionAttributeValues[':' + letters[letterIndex]] = object[updateKey]
+        if (keyIsReserved) {
+            expressionAttributeNames['#' + updateKey] = updateKey
+        }
         letterIndex++
     }
     const params = {
         TableName: 'Betsy_' + table,
-        Key: {
-            id: key
-        },
+        Key: key.constructor.name === 'Object' ? key : { id: key },
         UpdateExpression: updateExpression,
-        ExpressionAttributeValues: expressionAttributeValues
+        ExpressionAttributeValues: expressionAttributeValues,
+    }
+    if (Object.keys(expressionAttributeNames).length !== 0) {
+        params['ExpressionAttributeNames'] = expressionAttributeNames
     }
     try {
         await ddbDocClient.send(new UpdateCommand(params))
