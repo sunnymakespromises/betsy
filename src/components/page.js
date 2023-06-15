18f8a52c59640608778f44b356bd8bd21fbde11c
sunny! <66428984+sunnymakespromises@users.noreply.github.com>
2023-06-15 09:24:34 -0400

    added fade to dim

--- src/components/page.js
+++ src/components/page.js
@@ -3,9 +3,7 @@ import Conditional from "./conditional";
 export default function Page({ dim = null, children }) {
     return (
         <div className = 'page absolute w-full h-full p-8 z-0 animate__animated'>
-            <Conditional value = {dim}>
-                <div className = 'absolute -top-[100%] -left-[100%] w-[9999px] h-[9999px] bg-base-0 opacity-70 pointer-events-none z-10'/>
-            </Conditional>
+            <div id = 'dimmer' className = {'transition-all duration-main absolute -top-[100%] -left-[100%] w-[9999px] h-[9999px] bg-base-0 ' + (dim ? 'opacity-80' : 'opacity-0') + ' pointer-events-none z-10'}/>
             {children}
         </div>
     )
