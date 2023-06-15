180b712783091fb3a7e34d11e50b7ca37ac40caa
sunny! <66428984+sunnymakespromises@users.noreply.github.com>
2023-06-15 09:03:04 -0400

    added dimming capabilities to all pages

--- src/components/page.js
+++ src/components/page.js
@@ -1,6 +1,11 @@
-export default function Page({ children }) {
+import Conditional from "./conditional";
+
+export default function Page({ dim = null, children }) {
     return (
         <div className = 'page absolute w-full h-full p-8 z-0 animate__animated'>
+            <Conditional value = {dim}>
+                <div className = 'absolute -top-[100%] -left-[100%] w-[9999px] h-[9999px] bg-base-0 opacity-70 pointer-events-none z-10'/>
+            </Conditional>
             {children}
         </div>
     )
