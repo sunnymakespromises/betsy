a53f2e979d92208b323758ccbaf881aa34b33a23
sunny! <66428984+sunnymakespromises@users.noreply.github.com>
2023-06-17 06:09:06 -0400

    added explore result text

--- src/components/text.js
+++ src/components/text.js
@@ -12,6 +12,7 @@ export default function Text({ preset = 'main', styles, classes, children, ...ex
         'title': 'whitespace-nowrap select-none cursor-default font-main font-bold text-3xl md:text-4xl text-reverse-0 dark:text-base-0',
         'button': 'whitespace-nowrap select-none font-main font-semibold text-lg md:text-2xl text-reverse-0 dark:text-base-0',
         'profile-error': 'whitespace-nowrap select-none cursor-default font-main font-semibold text-xs md:text-xs text-reverse-0 dark:text-base-0',
+        'explore-result': 'whitespace-nowrap select-none font-main font-semibold text-2xl md:text-2xl text-reverse-0 dark:text-base-0',
     }
 
     const getOption = () => {
