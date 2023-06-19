07afbbab03ab6aaa0546efc5236cb0da83ba65df
sunny! <66428984+sunnymakespromises@users.noreply.github.com>
2023-06-19 07:35:51 -0400

    added uploadPicture and returns

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,12 +1,17 @@
 import { initializeApiData as _initializeApiData } from '../lib/api/initializeApiData'
+import { uploadPicture as _uploadPicture } from '../lib/api/uploadPicture'
 
 
 function useApi() {
     async function initializeApiData() {
-        await _initializeApiData()
+        return await _initializeApiData()
     }
 
-    return { initializeApiData }
+    async function uploadPicture(category, object, value) {
+        return await _uploadPicture(category, object, value)
+    }
+
+    return { initializeApiData, uploadPicture }
 }
 
 export { useApi }
