48898ce1708fcc0187f35759d385b8257748de80
sunny! <66428984+sunnymakespromises@users.noreply.github.com>
2023-06-18 22:43:20 -0400

    changed getData to include pictures

--- src/lib/getData.js
+++ src/lib/getData.js
@@ -25,7 +25,7 @@ async function getData() {
         }
     }
 
-    const sports = await getTable('Sports', ['id', 'name', 'slip_count'])
+    const sports = await getTable('Sports', ['id', 'name', 'picture', 'slip_count'])
     if (sports.length > 0) {
         response.data.sports = sports.sort((a, b) => (a.name.localeCompare(b.name)) || (a.slip_count - b.slip_count))
         response.statuses.sports = true
@@ -34,7 +34,7 @@ async function getData() {
         response.messages.sports = 'no sports found.'
     }
 
-    const competitions = await getTable('Competitions', ['id', 'name', 'sport', 'country', 'slip_count'])
+    const competitions = await getTable('Competitions', ['id', 'name', 'picture', 'sport', 'country', 'slip_count'])
     if (competitions.length > 0) {
         response.data.competitions = competitions.sort((a, b) => (a.name.localeCompare(b.name)) || (a.slip_count - b.slip_count))
         response.statuses.competitions = true
@@ -43,16 +43,16 @@ async function getData() {
         response.messages.competitions = 'no competitions found.'
     }
 
-    const events = await getTable('Events', ['id', 'name', 'competition', 'start_time', 'slip_count'])
+    const events = await getTable('Events', ['id', 'name', 'competition', 'competitors', 'start_time', 'slip_count'])
     if (events.length > 0) {
-        response.data.events = events.sort((a, b) => (a.start_time - b.start_time) || (a.slip_count - b.slip_count))
+        response.data.events = events.sort((a, b) => (a.start_time - b.start_time) || (a.slip_count - b.slip_count)).map(c => { return {...c, start_time: new Date(c.start_time * 1000).toLocaleString()} })
         response.statuses.events = true
     }
     else {
         response.messages.events = 'no events found.'
     }
 
-    const competitors = await getTable('Competitors', ['id', 'name', 'competitions', 'slip_count'])
+    const competitors = await getTable('Competitors', ['id', 'name', 'picture', 'competitions', 'slip_count'])
     if (competitors.length > 0) { 
         response.data.competitors = competitors.sort((a, b) => (a.name.localeCompare(b.name)) || (a.slip_count - b.slip_count))
         response.statuses.competitors = true
@@ -61,7 +61,7 @@ async function getData() {
         response.messages.competitors = 'no competitors found.'
     }
 
-    const users = await getTable('Users', ['id', 'username', 'display_name', 'picture'])
+    const users = await getTable('Users', ['id', 'username', 'picture', 'display_name'])
     if (users.length > 0) {
         response.data.users = users
         response.statuses.users = true
