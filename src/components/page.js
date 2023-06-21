6e21306fed6e7170ccf79d9ea05a6f1e2845d2fc
sunny! <66428984+sunnymakespromises@users.noreply.github.com>
2023-06-21 19:06:01 -0400

    silly me

--- src/components/page.js
+++ src/components/page.js
@@ -1,6 +1,6 @@
 export default function Page({ dim = null, children }) {
     return (
-        <div className = 'page absolute w-full h-full p-8 z-0 overflow-hidden animate__animated'>
+        <div className = 'page absolute w-full h-full p-main z-0 overflow-hidden animate__animated'>
             <div id = 'dimmer' className = {'transition-all duration-main absolute -top-[100%] -left-[100%] w-[9999px] h-[9999px] bg-base-0 ' + (dim ? 'opacity-80' : 'opacity-0') + ' pointer-events-none z-10'}/>
             {children}
         </div>
