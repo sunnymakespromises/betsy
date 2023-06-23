f356283ffeeacc50dd747aff6c08eb55fa6f3d22
sunny! <66428984+sunnymakespromises@users.noreply.github.com>
2023-06-23 01:16:54 -0400

    removed random ref

--- src/components/money.js
+++ src/components/money.js
@@ -1,14 +1,12 @@
-import { useRef } from 'react'
 import Text from './text'
 import { useCookies } from 'react-cookie'
 import calculateCurrency from '../lib/util/calculateCurrency'
 
 export default function Money({amount, classes, textClasses, shortened = false, ...extras}) {
     const [cookies,,] = useCookies()
-    const textRef = useRef()
     
     return (
-        <div ref = {textRef} className = {'money-container flex flex-row items-center gap-tiny md:gap-small' + (classes ? ' ' + classes : '')} {...extras}>
+        <div className = {'money-container flex flex-row items-center gap-tiny md:gap-small' + (classes ? ' ' + classes : '')} {...extras}>
             <Text classes = {'money-value' + (textClasses ? ' ' + textClasses : '')}>
                 {calculateCurrency(cookies['currency'], amount ? amount : '0.00', shortened)}
             </Text>
