794bf44d6f92482f818fd0ffb16fe97ab2a4f2fc
sunny! <66428984+sunnymakespromises@users.noreply.github.com>
2023-07-12 13:40:07 -0400

    useStore slight rework

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -2,27 +2,44 @@ import _ from 'lodash'
 import { useEffect, useRef, useMemo} from 'react'
 import { useCookies } from 'react-cookie'
 
-function useStore(category, shape, defaults) {
+function useStore(category, shape, defaults, options) {
     const storeName = useMemo(() => 'store_' + category, [category])
     const [cookies, setCookie,] = useCookies([storeName])
-    let store = useMemo(() => cookies[storeName] ? cookies[storeName] : [], [cookies[storeName]])
+    let store = useMemo(() => cookies[storeName] ? cookies[storeName] : getEmptyStore(), [cookies[storeName]])
     const storeRef = useRef()
     storeRef.current = store
 
     useEffect(() => {
         if (!cookies[storeName]) {
-            setCookie(storeName, defaults ? defaults : shape === 'array' ? [] : {})
+            setCookie(storeName, getEmptyStore())
         }
     }, [cookies])
 
     function addToStore(category, value) {
         if (shape === 'array') {
             value = category
-            setCookie(storeName, [...storeRef.current, value])
+            if (options?.duplicates === false) {
+                if (!(storeRef.current.some(item => _.isEqual(JSON.stringify(item), JSON.stringify(value))))) {
+                    if (value.constructor.name === 'Array') {
+                        setCookie(storeName, [...storeRef.current, ...value])
+                    }
+                    else {
+                        setCookie(storeName, [...storeRef.current, value])
+                    }
+                }
+            }
+            else {
+                setCookie(storeName, [...storeRef.current, value])
+            }
         }
         else {
             if (storeRef.current[category]) {
-                setCookie(storeName, {...storeRef.current, [category]: [...storeRef.current[category], value]})
+                if (value.constructor.name === 'Array') {
+                    setCookie(storeName, {...storeRef.current, [category]: [...storeRef.current[category], ...value]})
+                }
+                else {
+                    setCookie(storeName, {...storeRef.current, [category]: [...storeRef.current[category], value]})
+                }
             }
         }
     }
@@ -30,16 +47,36 @@ function useStore(category, shape, defaults) {
     function removeFromStore(category, value) {
         if (shape === 'array') {
             value = category
-            setCookie(storeName, storeRef.current.filter(item => !(_.isEqual(item, value))))
+            let newStore = storeRef.current.filter(item => !(_.isEqual(JSON.stringify(item), JSON.stringify(value))))
+            if (newStore.length > 0) {
+                setCookie(storeName, newStore)
+            }
+            else {
+                emptyStore()
+            }
         }
         else {
             if (storeRef.current[category]) {
-                setCookie(storeName, {...storeRef.current, [category]: storeRef.current[category].filter(item => !(_.isEqual(item, value)))})
+                let newStore = {...storeRef.current, [category]: storeRef.current[category].filter(item => !(_.isEqual(item, value)))}
+                if (Object.keys(newStore).some(category => newStore[category].length > 0)) {
+                    setCookie(storeName, {...storeRef.current, [category]: storeRef.current[category].filter(item => !(_.isEqual(JSON.stringify(item), JSON.stringify(value))))})
+                }
+                else {
+                    emptyStore()
+                }
             }
         }
     }
 
-    return { store: storeRef.current, addToStore, removeFromStore }
+    function emptyStore() {
+        setCookie(storeName, getEmptyStore())
+    }
+
+    function getEmptyStore() {
+        return defaults ? defaults : shape === 'array' ? [] : {}
+    }
+
+    return { store, addToStore, removeFromStore, emptyStore }
 }
 
 export { useStore }
