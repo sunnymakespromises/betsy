2f9aa7d686568863cbeed867a7962daadbadc578
sunny! <66428984+sunnymakespromises@users.noreply.github.com>
2023-08-05 16:21:59 -0400

    fixed issue with panels not refreshing

--- src/components/panel.js
+++ src/components/panel.js
@@ -1,4 +1,4 @@
-import { memo, useState } from 'react'
+import { memo, useEffect, useState } from 'react'
 import _ from 'lodash'
 import Text from './text'
 import { default as Icon } from './titleIcon'
@@ -21,8 +21,11 @@ const Panel = memo(function Panel({ title, icon, classes, children, parentId }) 
 }, (b, a) => b.title === a.title && b.classes === a.classes && _.isEqual(b.icon, a.icon) && _.isEqual(b.children, a.children))
 
 export const MultiPanel = memo(function MultiPanel({ config, parentId }) {
-    let [currentPanel, setCurrentPanel] = useState(config[0])
+    let [currentPanel, setCurrentPanel] = useState()
     const { isLandscape } = useWindowContext()
+    useEffect(() => {
+        setCurrentPanel(config[0])
+    }, [config])
     if (config) {
         if (isLandscape) {
             return (
@@ -34,19 +37,21 @@ export const MultiPanel = memo(function MultiPanel({ config, parentId }) {
             )
         }
         else {
-            let DOMId = parentId + '-panels'
-            return (
-                <div id = {DOMId} className = 'transition-colors duration-main w-full h-fit flex flex-col p-base gap-base bg-base-highlight rounded-base'>
-                    <div id = {DOMId + '-bar'} className = 'flex items-center gap-base'>
-                        <Map items = {config} callback = {(panel, index) => { return (
-                            <div key = {index} id = {panel.parentId + '-title'} onClick = {() => setCurrentPanel(panel)}>
-                                <Icon icon = {panel.icon} classes = {'!w-6 !h-6' + (currentPanel.key !== panel.key ? ' !text-text-highlight/killed hover:!text-primary-main cursor-pointer' : '')} parentId = {panel.parentId}/>
-                            </div>
-                        )}}/>
+            if (currentPanel) {
+                let DOMId = parentId + '-panels'
+                return (
+                    <div id = {DOMId} className = 'transition-colors duration-main w-full h-fit flex flex-col p-base gap-base bg-base-highlight rounded-base'>
+                        <div id = {DOMId + '-bar'} className = 'flex items-center gap-base'>
+                            <Map items = {config} callback = {(panel, index) => { return (
+                                <div key = {index} id = {panel.parentId + '-title'} onClick = {() => setCurrentPanel(panel)}>
+                                    <Icon icon = {panel.icon} classes = {'!w-6 !h-6' + (currentPanel.key !== panel.key ? ' !text-text-highlight/killed hover:!text-primary-main cursor-pointer' : '')} parentId = {panel.parentId}/>
+                                </div>
+                            )}}/>
+                        </div>
+                        {currentPanel.children}
                     </div>
-                    {currentPanel.children}
-                </div>
-            )
+                )
+            }
         }
     }
     return <></>
