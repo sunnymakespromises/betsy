7d71aaac5b1e8b4034dcf4ee0f1e6807dcfd491a
sunny! <66428984+sunnymakespromises@users.noreply.github.com>
2023-10-06 11:55:02 -0400

    forgot to cast totalDecimalOdds to Number

--- src/lib/util/calculateOdds.js
+++ src/lib/util/calculateOdds.js
@@ -2,12 +2,10 @@ import getFormattedOdds from './getFormattedOdds'
 
 export default function calculateOdds(format, expandedPicks) {
     expandedPicks = expandedPicks.filter(pick => pick.did_hit !== 'voided')
-    let totalDecimalOdds = (expandedPicks.map(pick => pick.outcome.odds).reduce((a, b) => a * b)).toFixed(2)
-    let totalAmericanOdds = totalDecimalOdds === 1 ? 100 : Number((totalDecimalOdds > 2 ? ((totalDecimalOdds - 1) * 100) : (-100 / (totalDecimalOdds - 1))).toFixed(0))
+    let totalDecimalOdds = Number((expandedPicks.map(pick => pick.outcome.odds).reduce((a, b) => a * b)).toFixed(2))
     let formattedOdds = getFormattedOdds(format, totalDecimalOdds)
     return { 
         string: formattedOdds,
-        american: totalAmericanOdds,
         decimal: totalDecimalOdds
     }
 }
